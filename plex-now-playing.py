57a619afffe0fc8ce36134d3c5755c0a0c50f843
colindouglas <colindouglas@gmail.com>
2019-10-30 19:49:18 -0300

    Better error handling

--- plex-now-playing.py
+++ plex-now-playing.py
@@ -11,11 +11,14 @@ import re
 WORKING_DIR = '~/Scripts/'              # Working directory (ending with /) where script saves tokens
 PLEX_SERVER = 'http://127.0.0.1:32400'  # Address of Plex server to query
 USERNAME = ''                           # Plex username to login, only required on first run
+PASSWORD = ''                           # Plex password to login, only required on first run
 
 # The location of the file that stores Plex token
-token_path = os.path.expanduser(WORKING_DIR) + '.plex_token_py'
+token_path = os.path.expanduser(WORKING_DIR) + '.plextoken'
 
-# Read in Plex token, update it if it's older than 1 day
+# Check if there's a token saved
+token = None
+username = None
 if os.path.exists(token_path):
     with open(token_path) as token_file:
         token_dict = next(csv.DictReader(token_file))
@@ -23,18 +26,12 @@ if os.path.exists(token_path):
     username = token_dict.get('username')
     token_last_update = datetime.strptime(token_dict.get('last_update'), '%Y-%m-%d %H:%M:%S.%f')
     since_last_update = datetime.now() - token_last_update
-    update_token = since_last_update.days >= 1
-else:
-    update_token = True
-    username = False
 
-if update_token:
-    if not username:
-        username = USERNAME
-    password = keyring.get_password('Plex-Now-Playing', username)
-    if password is None:
-        password = getpass()
-        keyring.set_password('Plex-Now-Playing', username, password)
+# Update the token if there wasn't one saved, or it's older than a day
+if not token or since_last_update.days >= 1:
+    username = username or USERNAME
+    password = keyring.get_password('Plex-Now-Playing', username) or PASSWORD
+    keyring.set_password('Plex-Now-Playing', username, password)
     plex_creds = [username, password]
     token_request = requests.post(
         url='https://plex.tv/users/sign_in.json',
@@ -44,22 +41,26 @@ if update_token:
             'X-Plex-Product': 'Plex-Now-Playing',
             'X-Plex-Version': '0.0.1'}
     )
-    token = json.loads(token_request.text)['user']['authToken']
-    token_data = {
-        'username': plex_creds[0],
-        'token': token,
-        'last_update': datetime.now()
-    }
-    with open(token_path, 'w') as token_file:
-        writer = csv.DictWriter(token_file, token_data.keys())
-        writer.writeheader()
-        writer.writerow(token_data)
+    if token_request.status_code < 300:
+        token = json.loads(token_request.text)['user']['authToken']
+        token_data = {
+            'username': plex_creds[0],
+            'token': token,
+            'last_update': datetime.now()
+        }
+        with open(token_path, 'w') as token_file:
+            writer = csv.DictWriter(token_file, token_data.keys())
+            writer.writeheader()
+            writer.writerow(token_data)
+    else:
+        print('Authentication problem')
 
 # Ask the local Plex server for the current sessions
-now_playing = requests.get(
-    url=PLEX_SERVER + '/status/sessions',
-    headers={'X-Plex-Token': token}
-)
+if token:
+    now_playing = requests.get(
+        url=PLEX_SERVER + '/status/sessions',
+        headers={'X-Plex-Token': token}
+    )
 
 # Parse the XML returned by Plex
 streams_xml = ET.fromstring(now_playing.text)
@@ -68,7 +69,8 @@ streams_xml = ET.fromstring(now_playing.text)
 def parse_stream_time(stream):
     start_time = stream.get('lastViewedAt', default=datetime.now())  # If there's no LastViewedAt, use current time
     if type(start_time) is str:
-        start_time = datetime.fromtimestamp(int(start_time))
+        start_time = int(start_time)
+        start_time = datetime.fromtimestamp(start_time)
     return start_time.strftime('%b %d, %I:%M')
 
 
@@ -97,27 +99,31 @@ if len(streams_xml):
                 season = "S" + re.search('[0-9]+', season_string).group(0)
             except AttributeError:
                 season = ''
-            print(start_time + ': ' + user + " // " +
-                  series_name + " - " + season + episode + " - " + episode_name
-                  )
+            print(
+                start_time + ': ' + user + " // " +
+                series_name + " - " + season + episode + " - " + episode_name
+            )
         # How to display Movies
         elif stream.get('type') == 'movie':
             movie_title = stream.get('title', default='Unknown Movie')
             movie_year = stream.get('originallyAvailableAt').split('-')[0]
             start_time = parse_stream_time(stream)
             user = parse_stream_user(stream)
-            print(start_time + ': ' + user + " // " +
-                  movie_title + " (" + movie_year + ")"
-                  )
+            print(
+                start_time + ': ' + user + " // " +
+                movie_title + " (" + movie_year + ")"
+                )
+        # How to display Music tracks
         elif stream.get('type') == 'track':
             track_title = stream.get('title', default='Unknown Song')
-            track_artist = stream.get('grandparentTitle')
-            # track_album = stream.get('parentTitle')
+            track_artist = stream.get('grandparentTitle', default='Unknown Artist')
+            # track_album = stream.get('parentTitle', default='Unknown Album')
             start_time = parse_stream_time(stream)
             user = parse_stream_user(stream)
-            print(start_time + ': ' + user + " // " +
-                  track_artist + " - " + track_title
-                  )
+            print(
+                start_time + ': ' + user + " // " +
+                track_artist + " - " + track_title
+                )
         else:
             start_time = parse_stream_time(stream)
             user = parse_stream_user(stream)
